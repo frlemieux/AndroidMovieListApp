65e870754b76eac210eb1211e629547b0139e59b
tagask <frederic.lemieux14@gmail.com>
2024-09-25 18:05:13 +0200

    adjust theme and add screenshot

--- app/src/main/java/com/lemieux/movielist/ui/theme/Theme.kt
+++ app/src/main/java/com/lemieux/movielist/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun MovieListTheme(
-    darkTheme: Boolean = false,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
